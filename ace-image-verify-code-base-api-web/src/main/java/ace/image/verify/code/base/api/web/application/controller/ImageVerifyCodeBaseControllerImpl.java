7d69dd78702fe5afabe7e3d1d83634b4e9fde84a
陈志杭 <279397942@qq.com>
2020-03-10 11:07:36 +0800

    init project

---
+++ ace-image-verify-code-base-api-web/src/main/java/ace/image/verify/code/base/api/web/application/controller/ImageVerifyCodeBaseControllerImpl.java
@@ -0,0 +1,42 @@
+package ace.image.verify.code.base.api.web.application.controller;
+
+import ace.fw.model.response.GenericResponseExt;
+import ace.fw.util.GenericResponseExtUtils;
+import ace.image.verify.code.base.api.controller.ImageVerifyCodeBaseController;
+import ace.image.verify.code.base.api.web.application.biz.imageverifycode.ImageVerifyCodeCheckBiz;
+import ace.image.verify.code.base.api.web.application.biz.imageverifycode.ImageVerifyCodeGetBiz;
+import ace.image.verify.code.define.base.model.request.CheckRequest;
+import ace.image.verify.code.define.base.model.request.GetImageVerifyCodeRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * @author Caspar
+ * @contract 279397942@qq.com
+ * @create 2020/1/19 11:37
+ * @description
+ */
+@RestController
+@Validated
+@Slf4j
+public class ImageVerifyCodeBaseControllerImpl implements ImageVerifyCodeBaseController {
+    @Autowired
+    private ImageVerifyCodeGetBiz getBiz;
+    @Autowired
+    private ImageVerifyCodeCheckBiz imageVerifyCodeCheckBiz;
+
+    @Override
+    public void get(@Valid GetImageVerifyCodeRequest request) {
+        getBiz.execute(request);
+    }
+
+    @Override
+    public GenericResponseExt<Boolean> check(@Valid CheckRequest request) {
+        imageVerifyCodeCheckBiz.execute(request);
+        return GenericResponseExtUtils.buildSuccessWithData(true);
+    }
+}
