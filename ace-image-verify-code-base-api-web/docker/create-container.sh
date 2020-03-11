e26216592fa0cb57b55d7c0520d325ff57054721
Casapr <279397942@qq.com>
2020-03-11 18:32:34 +0800

    验证码调整

--- ace-image-verify-code-base-api-web/docker/create-container.sh
+++ ace-image-verify-code-base-api-web/docker/create-container.sh
@@ -1,1 +1,7 @@
-docker run --restart=always -p 20003:20003 -d  --name ace-image-verify-code-base-api-web ace-image-verify-code-base-api-web:latest
+docker run \
+--restart=always \
+--net ace-network \
+-p 20003:20003 \
+-d  \
+--name ace-image-verify-code-base-api-web \
+ace-image-verify-code-base-api-web:latest
